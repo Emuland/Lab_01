7a8256f04a9fa7f45e08469e69c81eb225ddcbdb
drmage@mail.ru <105478386+Emuland@users.noreply.github.com>
2026-06-11 18:26:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RailwayStation lab_01 = new RailwayStation();
+			Console.WriteLine ("Default railway name: " + lab_01.StationName);
+			Console.WriteLine ("Default ticket cost: " + lab_01.TicketCost);
+			Console.WriteLine ("Default ticket cost: " + lab_01.TicketSoldCount);
+			lab_01.TicketCost = 150;
+			lab_01.SeatCount = 2000;
+			lab_01.TicketSoldCount = 1483;
+			Console.WriteLine ("New ticket cost: " + lab_01.TicketCost);
+			Console.WriteLine ("New ticket cost: " + lab_01.TicketSoldCount);
+			Console.WriteLine ("Unsold ticket revenue: " + lab_01.TicketUnsoldRevenue());
+			Console.ReadKey();
+		}
+	}
+}
---
+++ RailwayStation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_01
+{
+	class RailwayStation
+	{
+		private string stationName;
+		private double ticketCost;
+		private int seatCount;
+		private int ticketSoldCount;
+		public RailwayStation()
+		{
+			stationName = "Lab_01";
+			ticketCost = 0;
+			seatCount = 0;
+			ticketSoldCount = 0;
+		}
+		public string StationName
+		{
+			set {stationName = value;}
+			get {return stationName;}
+		}
+		public double TicketCost
+		{
+			set {ticketCost = value;}
+			get {return ticketCost;}
+		}
+		public int SeatCount
+		{
+			set {seatCount = value;}
+		}
+		public int TicketSoldCount
+		{
+			set {ticketSoldCount = value;}
+			get {return ticketSoldCount;}
+		}
+		public double TicketUnsoldRevenue()
+		{
+			double ticketUnsoldRevenue = 0;
+			ticketUnsoldRevenue = (seatCount-ticketSoldCount)*ticketCost;
+			return ticketUnsoldRevenue;
+		}
+	}
+}
