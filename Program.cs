6f9d1f2b55b5af04526135ece720c7217f7a8b98
drmage@mail.ru <105478386+Emuland@users.noreply.github.com>
2026-06-12 11:51:36 +0300

    cjrrect text error

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace Lab_01
 			RailwayStation lab_01 = new RailwayStation();
 			Console.WriteLine ("Default railway name: " + lab_01.StationName);
 			Console.WriteLine ("Default ticket cost: " + lab_01.TicketCost);
-			Console.WriteLine ("Default ticket cost: " + lab_01.TicketSoldCount);
+			Console.WriteLine ("Default ticket sold count: " + lab_01.TicketSoldCount);
 			lab_01.TicketCost = 150;
 			lab_01.SeatCount = 2000;
 			lab_01.TicketSoldCount = 1483;
 			Console.WriteLine ("New ticket cost: " + lab_01.TicketCost);
-			Console.WriteLine ("New ticket cost: " + lab_01.TicketSoldCount);
+			Console.WriteLine ("New ticket sold count: " + lab_01.TicketSoldCount);
 			Console.WriteLine ("Unsold ticket revenue: " + lab_01.TicketUnsoldRevenue());
 			Console.ReadKey();
 		}
